224533062b7b2722bdc36a1109ca3a4d4a851d1a
kozaku05 <kozaku0505@gmail.com>
2024-11-04 01:41:50 +0900

    ip取得からip.jsonに保存できるようにした ADMINからゲットできないエラー

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const fs = require("fs");
+const { json } = require("stream/consumers");
+const { error } = require("console");
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") req.url += "index.html";
@@ -18,8 +20,39 @@ const server = http.createServer((req, res) => {
   if (req.method === "POST") {
     let body = "";
     req.on("data", (chunk) => {
-        body += chunk.toString();
+      body += chunk.toString();
       console.log(body);
+    });
+    req.on("end", () => {
+      fs.readFile("ip.json", "utf-8", (err, data) => {
+        let jsonData = [];
+        let newData = JSON.parse(body);
+        if (!err && data) {
+          jsonData = JSON.parse(data);
+        }
+        jsonData.push(newData);
+        jsonData = JSON.stringify(jsonData, null);
+        fs.writeFile("ip.json", jsonData, "utf8", (err) => {
+          if (err) console.log(err);
+        });
+      });
+    });
+  }
+  if (req.method === "GET" && req.url === "/data") {
+    fs.readFile("ip.json", "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end(JSON.stringify({ error: "Error reading file" }));
+        return;
+      }
+      if (data) {
+        res.statusCode = 200;
+        res.end(data);
+      } else {
+        res.statusCode = 404;
+        res.end(JSON.stringify({ error: "Not Found" }));
+      }
     });
   }
 });
