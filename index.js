c16d7c7424869d1f5d6e700f1e2ce1beddb02501
kozaku05 <kozaku0505@gmail.com>
2024-11-04 14:00:11 +0900

    adminから取得できるようにした

--- index.js
+++ index.js
@@ -5,35 +5,17 @@ const { error } = require("console");
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") req.url += "index.html";
-    if (req.url === "/data") {
-      fs.readFile("ip.json", "utf-8", (err, data) => {
-        if (err) {
-          console.error(err);
-          res.statusCode = 500;
-          res.end(JSON.stringify({ error: "Error reading file" }));
-          return;
-        }
-        if (data) {
-          res.statusCode = 200;
-          res.end(data);
-        } else {
-          res.statusCode = 404;
-          res.end(JSON.stringify({ error: "Not Found" }));
-        }
-      });
-    } else {
-      fs.readFile(`./public-pages${req.url}`, (err, data) => {
-        if (err) {
-          res.statusCode = 404;
-          res.setHeader("Content-Type", "text/html");
-          res.end("<h1>404 not found</h1>");
-        } else {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "text/html");
-          res.end(data);
-        }
-      });
-    }
+    fs.readFile(`./public-pages${req.url}`, (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "text/html");
+        res.end("<h1>404 not found</h1>");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      }
+    });
   }
   if (req.method === "POST") {
     let body = "";
@@ -49,11 +31,28 @@ const server = http.createServer((req, res) => {
           jsonData = JSON.parse(data);
         }
         jsonData.push(newData);
-        jsonData = JSON.stringify(jsonData, null, 2);
+        jsonData = JSON.stringify(jsonData, null);
         fs.writeFile("ip.json", jsonData, "utf8", (err) => {
           if (err) console.log(err);
         });
       });
+    });
+  }
+  if (req.method === "GET" && req.url === "/data") {
+    fs.readFile("ip.json", "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end(JSON.stringify({ error: "Error reading file" }));
+        return;
+      }
+      if (data) {
+        res.statusCode = 200;
+        res.end(data);
+      } else {
+        res.statusCode = 404;
+        res.end(JSON.stringify({ error: "Not Found" }));
+      }
     });
   }
 });
